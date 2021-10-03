d2e286649c3e64e7a56ab655fd41e8e89aa35b78
Seymour <89657820+SaintUgodnik@users.noreply.github.com>
2021-10-03 13:21:25 +0300

    Commit

--- Program.cs
+++ Program.cs
@@ -5,8 +5,32 @@ namespace Snake
 {
     class Game
     {
+        public void MapGen()
+        {
+            int width = 50;
+            int height = 30;
+
+            for(int h = 0; h < width; h++)
+            {
+                for(int w = 0; w < height; w++)
+                {
+                    if (w ==0 ||w == 49) Console.Write("#");
+                    else if (h == 29) Console.Write("#\n");
+                    else if (w == 0 || h == 0) Console.Write("#");
+                    else Console.Write(" ");
+
+                }
+            }
+
+        }
+
+
         static void Main()
         {
+            Console.CursorVisible = false;
+
+            Game game = new Game();
+            game.MapGen();
 
         }
     }
